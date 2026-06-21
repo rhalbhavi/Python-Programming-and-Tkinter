3ad229a6dcb619882ea012f767e286bafed53c07
Riya <192779415+rhalbhavi@users.noreply.github.com>
2026-06-21 12:09:41 +0530

    Update and rename Strings/replace() - Replace Element in String with Another Element.py to Strings/String Methods/replace() - Replace Element in String with Another Element.py

--- Strings/replace() - Replace Element in String with Another Element.py
+++
@@ -1,9 +0,0 @@
-### Replace value in a string with another value ###
-
-
-msg = "I like Python programming"
-
-# replace() function
-print(msg.replace("like", "love"))
-
-# Output: I love Python programming
