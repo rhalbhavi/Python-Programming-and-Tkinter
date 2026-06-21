9b09b9926dd04a7969c4f880537e237e0e17fc79
Riya <192779415+rhalbhavi@users.noreply.github.com>
2026-06-21 11:41:46 +0530

    Update ArithmeticError.py

--- Try-Except-Finally Statements/Error Handling/ArithmeticError.py
+++ Try-Except-Finally Statements/Error Handling/ArithmeticError.py
@@ -1,12 +1,11 @@
-### Try-Except-Finally Statements - Arithmetic Error ###
-
+### Try-Except-Finally - Arithmetic Error ###
 
 try:
     b = int(input("Enter the value of the denominator: "))
     a = 10/b
     print (a)
-except (ArithmeticError, IOError, TypeError):
+# except ArithmeticError
+except (ArithmeticError, IOError, TypeError): # More errors are added here - this can be done
     print ("Arithmetic error occured")
 else:
     print ("Successfully executed")
-    
