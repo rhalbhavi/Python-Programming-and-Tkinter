163eb4d0c3818042f573e7b73a736b8472c66d3d
Riya <192779415+rhalbhavi@users.noreply.github.com>
2026-06-21 11:22:32 +0530

    Update and rename Try-Except-Finally Statements with Else statement.py to try-except-finally with else.py

--- Try-Except-Finally Statements/try-except-finally with else.py
+++ Try-Except-Finally Statements/try-except-finally with else.py
@@ -1,19 +1,20 @@
-### Try-Except-Finally Statements - Check if Even or Odd Number with Else Statement ###
+### try-except-finally Statements with else Statement ###
 
-
+# try:
 try:
     num = int(input("Enter a number: "))
-# if value entered is not an integer, show error
+# except:
 except:
     print ("Enter only an integer value")
+# else:
 else:
-    # define a formula for even/odd numbers
     even_or_odd = num%2==0
-    # if number is even (even_or_odd = true)
+    # if condition:
     if even_or_odd:
         print ("Number entered is even")
-    # else if number is odd (even_or_odd = false)
+    # else:
     else:
         print ("Number entered is odd")
+# finally:
 finally:
     print ("Program executed")
