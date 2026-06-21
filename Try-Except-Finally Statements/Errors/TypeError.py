3b61120e7324458d93173f39ee014fdda0e1f94f
Riya <192779415+rhalbhavi@users.noreply.github.com>
2026-06-21 11:39:13 +0530

    Update and rename Try-Except-Finally Statements/TypeError.py to Try-Except-Finally Statements/Errors/TypeError.py

--- Try-Except-Finally Statements/Errors/TypeError.py
+++ Try-Except-Finally Statements/Errors/TypeError.py
@@ -1,14 +1,14 @@
-### Try-Except-Finally Statements - TypeError ###
-
+### Try-Except-Finally - TypeError ###
 
 num = [5, 6, 'a', 7, 8, 9]
 b = 15
+
 for x in num:
     try:
         c = b/x
         print (c)
     except ZeroDivisionError:
         print("Cannot be divided by 0")
-    # If value is not an integer, show TypeError
+    # except TypeError:
     except TypeError:
         print ("Enter only integer value")
