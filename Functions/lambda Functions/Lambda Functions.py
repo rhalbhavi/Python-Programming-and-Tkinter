0d219c6e84b431c114df769dd67379c440201efc
Riya <192779415+rhalbhavi@users.noreply.github.com>
2026-06-21 13:12:09 +0530

    Update Lambda Functions.py

--- Functions/lambda Functions/Lambda Functions.py
+++ Functions/lambda Functions/Lambda Functions.py
@@ -1,8 +1,7 @@
 ### Lambda Functions ###
-# anonymous function - defined without a name
+  # Anonymous function - Defined without a name
 
-## lambda arguments: expression
-# function body - can contain any number of arguments, 1 expression
+# Function_Name = lambda Arguments: Expression
 add = lambda x, y: x + y
+# function body - can contain any number of arguments, 1 expression
 print("Sum = ", add(3, 6)) # Output: Sum =  9
-
