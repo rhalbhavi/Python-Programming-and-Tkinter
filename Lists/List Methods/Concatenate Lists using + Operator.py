2104682418602cd459990e5255eac0f5a0f2cac8
Riya <192779415+rhalbhavi@users.noreply.github.com>
2026-06-21 09:53:01 +0530

    Update and rename Lists/Add:Concatenate (+) Lists.py to Lists/List Methods/Concatenate Lists using + Operator.py

--- Lists/List Methods/Concatenate Lists using + Operator.py
+++ Lists/List Methods/Concatenate Lists using + Operator.py
@@ -1,12 +1,18 @@
-### Merge/Concatenate Lists ###
+### Join/Concatenate (+) Lists using + Operator ###
 
-# Merge/Concatenate 2 lists
+# Initialize 2 Lists
 a = [10,20,30,40,50]
 b = [60,70,80,90,100]
+
+# List1 + List2 : Concatenate the lists
 print (a + b) # Output: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
 
-# Merge/Concatenate 3 lists
+# -------------------------------------------------------------------
+
+# Initialize 3 Lists
 a = [10,20,30,40,50]
 b = [60,70,80,90,100]
 c = [1,2,3.3,4.45,6]
+
+# Concatenate the lists
 print (a + b + c) # Output: [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 1, 2, 3.3, 4.45, 6]
