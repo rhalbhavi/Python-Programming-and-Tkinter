28604065b2e7edaef314eeafff7fb695846fcb74
Riya <192779415+rhalbhavi@users.noreply.github.com>
2026-06-21 09:48:34 +0530

    Update and rename Lists/append() - Append Values to List.py to Lists/List Methods/append() - Append Values to List.py

--- Lists/List Methods/append() - Append Values to List.py
+++ Lists/List Methods/append() - Append Values to List.py
@@ -1,13 +1,10 @@
 ### append() - Append Values to List ###
 
+# Initialize List
 List1 = ["carrot", "potato", "lettuce", "beans", "corn"]
+
 # Add/append a value to the list
 List1.append("capsicum")
+
 # Print updated list
 print (List1)
-
-List2 = ["a", "b", "c", "d", "e"]
-# Add/append a value to the list
-List2.append("f")
-# Print updated list
-print (List2)
