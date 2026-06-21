425476fcd347c86de7bb098887350629775d08ad
Riya <192779415+rhalbhavi@users.noreply.github.com>
2026-06-21 11:52:23 +0530

    Update Dictionaries.py

--- Dictionaries/Initializing Dictionaries/Dictionaries.py
+++ Dictionaries/Initializing Dictionaries/Dictionaries.py
@@ -2,21 +2,24 @@
 # key-value pair (key:value)
 # Values in a dictionary can be of different types - number, string, tuple)
 
-# Define dictionary
+
+# Initialize dictionary
+  # Dictionary = {key: value, key: value, ...}
 Dict = {1: 'Python', 2: 'Java', 3: 'C++'}
 
-## print dictionary
+## Print dictionary
 print(Dict) # Output: {1: 'Python', 2: 'Java', 3: 'C++'}
 
-## create a dictionary using dict() function
+
+## dict([(key, value), (key, value)]) -- Create a dictionary
 dict = dict([(1, 'apple'), (2, 'ball')])
+
 print (dict) # Output: {1: 'apple', 2: 'ball'}
 
-## print keys and values in dictionary
-Dict2 = {"Key1": "Hello", "Key2": "Goodbye", "Key3": "See you later"}
 
-# keys() - print keys in dictionary
-print(Dict2.keys()) # Output: dict_keys(['Key1', 'Key2', 'Key3'])
+# Dict.keys() -- Print keys in dictionary
+print(Dict.keys()) # Output: dict_keys(['Key1', 'Key2', 'Key3'])
 
-# values() - print values in dictionary
-print(Dict2.values()) # Output: dict_values(['Hello', 'Goodbye', 'See you later'])
+
+# Dict.values() -- Print values in dictionary
+print(Dict.values()) # Output: dict_values(['Hello', 'Goodbye', 'See you later'])
