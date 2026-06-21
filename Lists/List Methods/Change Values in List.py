0d2646a8f89f7515bbbca6cd23457b3363aaecdb
Riya <192779415+rhalbhavi@users.noreply.github.com>
2026-06-21 09:50:55 +0530

    Update Change Values in List.py

--- Lists/List Methods/Change Values in List.py
+++ Lists/List Methods/Change Values in List.py
@@ -3,7 +3,7 @@
 # Initialize List
 list = [1,2,3,4,5,6,7,8]
 
-# Assign a different value to an index
+# List[index] = value - Assign a different value to an index
 list[5]="AAA"
 
 # Print the updated list
