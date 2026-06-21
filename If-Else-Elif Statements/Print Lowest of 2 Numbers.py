69481f7e49647ab593cf44217fe9858f997f4c56
Riya <192779415+rhalbhavi@users.noreply.github.com>
2026-06-21 09:19:26 +0530

    Update Print Lowest of 2 Numbers.py

--- If-Else-Elif Statements/Print Lowest of 2 Numbers.py
+++ If-Else-Elif Statements/Print Lowest of 2 Numbers.py
@@ -1,10 +1,15 @@
+## if-else-elif Statement ##
+
 n1 = int(input("Enter a number: "))
 n2 = int(input("Enter another number: "))
 
+# if (condition)
 if (n1<n2):
     print (n1, "<", n2)
+# elif (condition)
 elif (n1==n2):
     print (n1, "=", n2)
+# else (condition)
 else:
     print (n2, "<", n1)
 
